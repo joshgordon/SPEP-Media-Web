6f294e0f9858172a4aa25cec2b935f8d299f22b3
Josh Gordon <github@joshgordon.net>
2015-03-08 16:05:24 -0400

    Cofig file and giving portion of the webpage.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+$DBServer="localhost";
+$DBUser="web"; 
+$DBPass="H4x03r"; 
+$DBName="spep_web"; 
+?> 
---
+++ give.php
@@ -0,0 +1,46 @@
+<style> 
+blockquote {
+  background: #f9f9f9; 
+  border-left 10px solid #ccc; 
+  margin: 1.5em 10px; 
+  padding: 0.5em 10px;
+  font-size:85%; 
+} 
+
+.give-message { 
+  font-size:100%; 
+  font-weight: bold; 
+}
+
+.give-hr { 
+  margin-top: 85px; 
+  margin-bottom: 5px; 
+}
+
+a:link { 
+  color: #D51566; 
+}
+
+a:visited { 
+  color: #D51566; 
+}
+
+</style>
+
+<hr class="give-hr" /> 
+<p class="give-message">If you enjoy this ministry, please consider giving a gift to help us continue.</p> 
+
+<ul>
+  <li>Check - Mail it to: 
+    <blockquote>
+      Severna Park EP Church <br />
+      110 Ritchie Highway <br /> 
+      Pasadena, MD 21122 <br /> 
+    </blockquote> 
+  </li>
+  <li>Give online 
+  <a href="https://spep.ccbchurch.com/trx_submit.php?type=public_gift">
+  as a Guest</a>; or if you attend SPEP, you can  
+  <a href="https://spep.ccbchurch.com/trx_give.php">Log In</a>
+  to SPEPConnect to give. </li>
+</ul>
