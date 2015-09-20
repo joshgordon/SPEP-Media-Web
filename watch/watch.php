f2dd5a55984bc29f4ca6181442cfbc82b71555c6
Josh Gordon <github@joshgordon.net>
2015-09-19 22:40:22 -0400

    Adding watch.php

---
+++ watch/watch.php
@@ -0,0 +1,22 @@
+<?php
+include "../assets.php";
+generateHeader("Video Stream", True);
+?>
+
+<div id="boxcast-widget-9-20-15-9-30-service"><script type="text/javascript"> var s = document.createElement('script'); var a = String.fromCharCode; var h = (('https:' == document.location.protocol) ? 'https:' : 'http:'); s.setAttribute('data-action', 'boxcast-widget-load'); s.setAttribute('data-handle', '9-20-15-9-30-service'); s.setAttribute('data-options', 'version=3'); s.src = h + '//assets.boxcast.com/widget.js'; document.getElementsByTagName('head')[0].appendChild(s); </script></div>
+
+<div style="font-size: 24pt">Please Note:</div>
+<div>We are testing a new streaming system. In our own testing, we have 
+found it to work fantastically. If you are having issues, please 
+<a href="http://spepchurch.org/contactus/contact_media.html">let us know</a>
+so we can work to fix them.</div>
+
+<div>If you would like to go back to the old site, you can do so by clicking
+<a href="http://www.ustream.tv/embed/11308704">here</a>. Note that this is a
+temporary fix, and will not be available for much longer. Therefore, it is 
+imperative for you to let us know what issues you are having so we can fix
+them.</div>
+
+<?php include "../give.php"; ?> 
+
+<?php generateFooter(True); ?> 
